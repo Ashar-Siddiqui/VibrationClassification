e8f97dee60f4effe34e7c514253d7c992119986e
Ashar Siddiqui <ashar.siddiqui61@gmail.com>
2022-07-17 17:23:36 -0700

    Put test code

--- helloworld.py
+++ helloworld.py
@@ -2,10 +2,21 @@ import sklearn
 from sklearn.neural_network import MLPClassifier
 from sklearn import datasets
 import numpy as np
-
+from os.path import dirname, join
 
 def display_col():
-    pass
+    data = np.genfromtxt(join(dirname(__file__), 'test.csv'), delimiter=',', skip_header=1, dtype=np.float32)
+
+    input_voltage = data[:, 0]
+    vibration_1 = data[:, 2]
+    vibration_2 = data[:, 3]
+    vibration_3 = data[:, 4]
+
+    #accessing columns
+    #print(data[:, 2])
+    #accessing rows
+    #print(data[3:])
+    #print(data)
 
 if __name__ == '__main__':
     display_col()
